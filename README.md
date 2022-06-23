ab2020f2633f5b4cd3d2d2b347b70b6497699de2
Mahesh Sankhala <maheshsainni@gmail.com>
2022-06-23 17:24:49 +0530

    insta.sh updated

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Raspberry pi dotfiles
 
 This repo contains the dotfiles for raspberry pi for easy setup
+
+## Install
+
+```bash
+git clone https://github.com/msankhala/rpi-dotfiles.git ~/dotfiles
+```
--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 export DOTFILES_HOME=~/dotfiles
 
+# install bpkg https://github.com/bpkg/bpkg
+# curl -Lo- "https://raw.githubusercontent.com/bpkg/bpkg/master/setup.sh" | bash
+
 # Check if ~/.bashrc file exits, if exits take its back up and create a symbolic
 # link to the dotfiles
 if [ -f ~/.bashrc ]; then
